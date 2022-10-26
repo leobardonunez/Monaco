9f600d7fb8dc7d15bfabbc98efe568eee85321be
Leobardo Nuñez <Leobardo_nugue@hotmail.com>
2022-10-26 16:03:08 -0600

    Sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+/*import React , {Component} from "react";*/
+import React from "react";
+
+
+function Sidebar(props) {
+    return <h2>{props.msg}</h2>;
+}
+
+/* class Sidebar extends Component{
+   render(){
+    return <h2>Hola soy un componente</h2>
+   }
+} */
+
+
+
+export default Sidebar;
