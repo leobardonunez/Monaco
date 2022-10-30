07db11520505aff745f34d3ab91d7d7b159d7fc7
Leobardo nuñez <Leobardong@gmail.com>
2022-10-30 14:14:22 -0700

    Se añadio archivo routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch ,Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 //Import components
-import Table from "./components/Table";
-import CardDashboard from "./components/CardDashboard";
 import Navigation from "./components/Navigation";
 import Sidebar from "./components/Sidebar";
-import Almacen from "../src/pages/Almacen";
-import Layout from "./pages/Layout";
-import Dashboard from "./pages/Dashboard";
-import Error404 from "./pages/Error404";
+import Routes from "./pages/Routes";
 
 function App() {
   return (
-    <div className="App">
-      {/*    <header className="App-header">
-
-      </header> */}      
+    <div className="App">      
       <Router>
       <Navigation />
-      <Sidebar />
-        <Switch>
-          <Route path="/" exact component={Dashboard} />
-          <Route path="/almacen" exact component={Almacen} />
-          <Route path="/layout" exact component={Layout} />
-          <Route path="*" component={Error404}/>
-        </Switch>
-      </Router>
+      <Sidebar />        
+          <Routes/>             
+      </Router>    
     </div>
   );
 }
